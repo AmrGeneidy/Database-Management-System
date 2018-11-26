3e8630effe631ad7f63fdb3a6c0291d4e683c074
MHMD_Alaa <mhmdalaa333@gmail.com>
2018-11-27 01:02:34 +0200

    Insert finished without debugging

--- src/eg/edu/alexu/csd/oop/db/cs28/SQLDatabase.java
+++ src/eg/edu/alexu/csd/oop/db/cs28/SQLDatabase.java
@@ -1,27 +1,43 @@
 package eg.edu.alexu.csd.oop.db.cs28;
 
 import eg.edu.alexu.csd.oop.db.Database;
+import eg.edu.alexu.csd.oop.db.cs28.Parser.returnType;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class SQLDatabase implements Database {
-    @Override
-    public String createDatabase(String databaseName, boolean dropIfExists) {
-        return null;
-    }
+	@Override
+	public String createDatabase(String databaseName, boolean dropIfExists) {
+		return null;
+	}
 
-    @Override
-    public boolean executeStructureQuery(String query) throws SQLException {
-        return false;
-    }
+	@Override
+	public boolean executeStructureQuery(String query) throws SQLException {
+		return false;
+	}
 
-    @Override
-    public Object[][] executeQuery(String query) throws SQLException {
-        return new Object[0][];
-    }
+	@Override
+	public Object[][] executeQuery(String query) throws SQLException {
+		return new Object[0][];
+	}
 
-    @Override
-    public int executeUpdateQuery(String query) throws SQLException {
-        return 0;
-    }
+	@Override
+	public int executeUpdateQuery(String query) throws SQLException {
+		int rowsCount = 0;
+		Parser parser = new Parser();
+		if (!parser.executeUpdateQuery(query)) {
+			throw new SQLException();
+		}
+		HashMap<returnType, Object> map = parser.map;
+		if ((boolean)map.get(returnType.ISINSERT)) {
+			rowsCount = ModifyTable.insert(null/*workSpacePath*/,map);
+		}else if ((boolean)map.get(returnType.ISUPDATE)) {
+			rowsCount = ModifyTable.update(null/*workSpacePath*/,map);
+		}else if ((boolean)map.get(returnType.ISDELETE)) {
+			rowsCount = ModifyTable.delete(null/*workSpacePath*/,map);
+		}
+	
+		return rowsCount;
+	}
 }
