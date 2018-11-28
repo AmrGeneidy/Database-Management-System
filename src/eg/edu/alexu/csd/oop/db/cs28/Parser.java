c13d8c7996e9de9ee2521d2c8075cf42d08a920f
AmrGeneidy <amr.geneidy.98@gmail.com>
2018-11-28 23:34:21 +0200

    fixing Space-Bugs

--- src/eg/edu/alexu/csd/oop/db/cs28/Parser.java
+++ src/eg/edu/alexu/csd/oop/db/cs28/Parser.java
@@ -12,52 +12,48 @@ public class Parser {
     public boolean executeStructureQuery(String query) {
         boolean matched = false;
         map = new HashMap<>();
-        String[] queries = {"CREATE DATABASE", "DROP DATABASE", "CREATE TABLE", "DROP TABLE"};
-        for (String q : queries) {
-            if (regexChecker(q, query)) {
-                String s = query.substring(lastMatchedIndex + 1);
-                switch (q) {
-                    case "CREATE DATABASE":
-                        s = s.trim();
-                        map.put(returnType.ISDATABASE, true);
-                        map.put(returnType.ISCREATE, true);
-                        map.put(returnType.NAME, s);
-                        break;
-                    case "DROP DATABASE":
-                        s = s.trim();
-                        map.put(returnType.ISDATABASE, true);
-                        map.put(returnType.ISCREATE, false);
-                        map.put(returnType.NAME, s);
-                        break;
-                    case "CREATE TABLE":
-                        if (s.indexOf('(') != -1) {
-                            map.put(returnType.NAME, s.substring(0, s.indexOf('(')));
-                            s = s.substring(s.indexOf('(') + 1, s.indexOf(')'));
-                            String[] nameAndType = s.split(",");
-                            String[] colName = new String[nameAndType.length];
-                            String[] colType = new String[nameAndType.length];
-                            for (int i = 0; i < nameAndType.length; i++) {
-                                String[] temp = nameAndType[i].trim().split(" ");
-                                colName[i] = temp[0];
-                                colType[i] = temp[1];
-                            }
-                            map.put(returnType.COLNAME, colName);
-                            map.put(returnType.COLTYPE, colType);
-                        } else {
-                            return false;
-                        }
-                        map.put(returnType.ISDATABASE, false);
-                        map.put(returnType.ISCREATE, true);
-                        break;
-                    case "DROP TABLE":
-                        s = s.trim();
-                        map.put(returnType.ISDATABASE, false);
-                        map.put(returnType.ISCREATE, false);
-                        map.put(returnType.NAME, s);
-                        break;
+        if (regexChecker("CREATE[\\s]+DATABASE", query)) {
+            String s = query.substring(lastMatchedIndex + 1);
+            s = s.trim();
+            map.put(returnType.ISDATABASE, true);
+            map.put(returnType.ISCREATE, true);
+            map.put(returnType.NAME, s);
+            matched = true;
+        } else if (regexChecker("DROP[\\s]+DATABASE", query)) {
+            String s = query.substring(lastMatchedIndex + 1);
+            s = s.trim();
+            map.put(returnType.ISDATABASE, true);
+            map.put(returnType.ISCREATE, false);
+            map.put(returnType.NAME, s);
+            matched = true;
+        } else if (regexChecker("CREATE[\\s]+TABLE", query)) {
+            String s = query.substring(lastMatchedIndex + 1);
+            if (s.indexOf('(') != -1) {
+                map.put(returnType.NAME, s.substring(0, s.indexOf('(')));
+                s = s.substring(s.indexOf('(') + 1, s.indexOf(')'));
+                String[] nameAndType = s.split(",");
+                String[] colName = new String[nameAndType.length];
+                String[] colType = new String[nameAndType.length];
+                for (int i = 0; i < nameAndType.length; i++) {
+                    String[] temp = nameAndType[i].trim().replaceAll("[\\s]+", " ").split(" ");
+                    colName[i] = temp[0];
+                    colType[i] = temp[1];
                 }
-                matched = true;
+                map.put(returnType.COLNAME, colName);
+                map.put(returnType.COLTYPE, colType);
+            } else {
+                return false;
             }
+            map.put(returnType.ISDATABASE, false);
+            map.put(returnType.ISCREATE, true);
+            matched = true;
+        } else if (regexChecker("DROP[\\s]+TABLE", query)) {
+            String s = query.substring(lastMatchedIndex + 1);
+            s = s.trim();
+            map.put(returnType.ISDATABASE, false);
+            map.put(returnType.ISCREATE, false);
+            map.put(returnType.NAME, s);
+            matched = true;
         }
         if (matched) {
             return true;
@@ -71,7 +67,9 @@ public class Parser {
             String[] colCollector = s.split("FROM");
             if (!colCollector[0].trim().equals("*")) {
                 String[] col = colCollector[0].split(",");
-                for (String c : col) c = c.trim();
+                for (int i = 0; i < col.length; i++) {
+                    col[i] = col[i].trim();
+                }
                 if (col[0].replaceAll("\\s+", "").equals("")) return false;
                 map.put(returnType.COLNAME, col);
             }
@@ -86,45 +84,40 @@ public class Parser {
     public boolean executeUpdateQuery(String query) {
         boolean matched = false;
         map = new HashMap<>();
-        String[] queries = {"INSERT INTO", "DELETE FROM", "UPDATE"};
-        for (String q : queries) {
-            if (regexChecker(q, query)) {
-                String s = query.substring(lastMatchedIndex + 1);
-                switch (q) {
-                    case "INSERT INTO":
-                        ArrayList<Object> col = new ArrayList<>();
-                        ArrayList<Object> val = new ArrayList<>();
-                        regexUpdateQuery(s, "VALUES", col, val);
-                        map.put(returnType.COLNAME, col.toArray());
-                        map.put(returnType.COLVALUES, val.toArray());
-                        map.put(returnType.ISUPDATE, false);
-                        map.put(returnType.ISDELETE, false);
-                        map.put(returnType.ISINSERT, true);
-                        break;
-                    case "DELETE FROM":
-                        String[] conditionCollector = s.split("WHERE");
-                        map.put(returnType.NAME, conditionCollector[0].trim());
-                        conditionFinder(s, conditionCollector);
-                        map.put(returnType.ISUPDATE, false);
-                        map.put(returnType.ISDELETE, true);
-                        map.put(returnType.ISINSERT, false);
-                        break;
-                    case "UPDATE":
-                        String[] collector = s.split("WHERE");
-                        s = collector[0];
-                        ArrayList<Object> column = new ArrayList<>();
-                        ArrayList<Object> value = new ArrayList<>();
-                        updateSetter(s, column, value);
-                        map.put(returnType.COLNAME, column.toArray());
-                        map.put(returnType.COLVALUES, value.toArray());
-                        conditionFinder(s, collector);
-                        map.put(returnType.ISUPDATE, true);
-                        map.put(returnType.ISDELETE, false);
-                        map.put(returnType.ISINSERT, false);
-                        break;
-                }
-                matched = true;
-            }
+        if (regexChecker("INSERT[\\s]+INTO", query)) {
+            String s = query.substring(lastMatchedIndex + 1);
+            ArrayList<Object> col = new ArrayList<>();
+            ArrayList<Object> val = new ArrayList<>();
+            regexUpdateQuery(s, "VALUES", col, val);
+            map.put(returnType.COLNAME, col.toArray());
+            map.put(returnType.COLVALUES, val.toArray());
+            map.put(returnType.ISUPDATE, false);
+            map.put(returnType.ISDELETE, false);
+            map.put(returnType.ISINSERT, true);
+            matched = true;
+        } else if (regexChecker("DELETE[\\s]+FROM", query)) {
+            String s = query.substring(lastMatchedIndex + 1);
+            String[] conditionCollector = s.split("WHERE");
+            map.put(returnType.NAME, conditionCollector[0].trim());
+            conditionFinder(s, conditionCollector);
+            map.put(returnType.ISUPDATE, false);
+            map.put(returnType.ISDELETE, true);
+            map.put(returnType.ISINSERT, false);
+            matched = true;
+        } else if (regexChecker("UPDATE", query)) {
+            String s = query.substring(lastMatchedIndex + 1);
+            String[] collector = s.split("WHERE");
+            s = collector[0];
+            ArrayList<Object> column = new ArrayList<>();
+            ArrayList<Object> value = new ArrayList<>();
+            updateSetter(s, column, value);
+            map.put(returnType.COLNAME, column.toArray());
+            map.put(returnType.COLVALUES, value.toArray());
+            conditionFinder(s, collector);
+            map.put(returnType.ISUPDATE, true);
+            map.put(returnType.ISDELETE, false);
+            map.put(returnType.ISINSERT, false);
+            matched = true;
         }
         if (matched) {
             return true;
@@ -185,7 +178,7 @@ public class Parser {
                 if (condition.contains(op)) {
                     String[] operands = condition.split(op);
                     for (int i = 0; i < operands.length; i++) {
-                        Matcher matcher = Pattern.compile("[A-Za-z_]+").matcher(operands[i]);
+                        Matcher matcher = Pattern.compile("[A-Za-z1-9_]+").matcher(operands[i]);
                         if (matcher.find()) {
                             operands[i] = matcher.group().trim();
                         }
