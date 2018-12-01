39ca566da6a5d1f85c87e28f4966c44d0cf1eeb9
MHMD_Alaa <mhmdalaa333@gmail.com>
2018-12-01 22:48:48 +0200

    conflict commit

--- src/eg/edu/alexu/csd/oop/db/cs28/ModifyTable.java
+++ src/eg/edu/alexu/csd/oop/db/cs28/ModifyTable.java
@@ -40,6 +40,13 @@ public class ModifyTable {
 			processFailed = true;
 		}
 
+		for (int i = 0; i < values.length; i++) {
+			if (values[i].contains("'")) {
+				values[i] = values[i].replaceAll("'", "");
+			}
+		}
+		
+		
 		String[] itemsRow = new String[colNames.length];
 		String[] dataTypes = table.getColsDataTypes();
 		if (allColsCase) {
