304f27807cbcfdf13ea88f516b3b988fd7e11d0c
Ahmed Ashraf <engahmedashraf2210@gmail.com>
2018-12-02 20:15:27 +0200

    main is added, table remains

---
+++ src/eg/edu/alexu/csd/oop/db/cs28/Console.java
@@ -0,0 +1,92 @@
+package eg.edu.alexu.csd.oop.db.cs28;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Console {
+
+	private JFrame frame;
+	private String query;
+	private int begin = 0;
+	private Facade database = new Facade(new SQLDatabase());
+	private Table table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Console window = new Console();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Console() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 575, 419);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Berlin Sans FB", Font.PLAIN, 18));
+		textArea.setForeground(Color.WHITE);
+		textArea.setBackground(Color.BLACK);
+		textArea.setBounds(10, 11, 539, 369);
+		panel.setLayout(new BorderLayout());
+		JScrollPane scroll = new JScrollPane(textArea); // place the JTextArea in a scroll pane
+		panel.add(scroll, BorderLayout.CENTER);
+		panel.setBackground(Color.BLACK);
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+
+		textArea.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if (arg0.getKeyCode() == 10) {
+					query = textArea.getText().substring(begin, textArea.getText().length());
+					begin = textArea.getText().length() + 1;
+					if (query.equalsIgnoreCase("clear")) {
+						textArea.setText("");
+						begin = 0;
+					} else {
+						try {
+							database.executeQuery(query);
+						} catch (Exception e) {
+							// TODO: handle exception
+							textArea.setText(textArea.getText() + "\n" + e.getMessage());
+						}
+
+					}
+
+				}
+			}
+		});
+
+	}
+
+}
--- src/eg/edu/alexu/csd/oop/db/cs28/Main.java
+++ src/eg/edu/alexu/csd/oop/db/cs28/Main.java
@@ -1,38 +1,23 @@
 package eg.edu.alexu.csd.oop.db.cs28;
 
-import java.io.Console;
-
+import java.io.IOException;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
- 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
 public class Main extends Application {
-
-    Button button;
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-    	
-    	
-    	
-    	VBox layout = new VBox(20);
-    	
-    	Scene scene = new Scene(layout, 500, 500);
-    	
-    	
-    	Console console = System.console();
-    	primaryStage.setScene(scene);
-    	primaryStage.show();
-    	
-        		
-    }
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ConsoleGUI.fxml"));
+        primaryStage.setTitle("Console");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
