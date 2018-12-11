b53ddb9b89b1b3497e12626899bca820b34595f9
AmrGeneidy <amr.geneidy.98@gmail.com>
2018-12-11 20:57:28 +0200

    preparing GUI elements

--- src/eg/edu/alexu/csd/oop/jdbc/cs50/Main.java
+++ src/eg/edu/alexu/csd/oop/jdbc/cs50/Main.java
@@ -1,7 +1,17 @@
 package eg.edu.alexu.csd.oop.jdbc.cs50;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Properties;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
@@ -9,16 +19,27 @@ import javafx.scene.Scene;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("MainDesign.fxml"));
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainDesign.fxml"));
         primaryStage.setTitle("JDBC");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+        Driver driver = new DriverImp();
+        Properties info = new Properties();
+        File dbDir = new File("workspace");
+        info.put("path", dbDir.getAbsoluteFile());
+        Connection connection;
+        try {
+            connection = driver.connect("jdbc:xmldb://localhost", info);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
